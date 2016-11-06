9d01f3c933ad159da40227e8ca2ba0d21d140159
CosmicCoder96 <abhinav.khare31@gmail.com>
2016-11-07 01:20:24 +0530

    MongoDB connections
    Switched all the data extractions which were being delivered through loading
    the json files directly to database connections. i.e. data is read and added from/to the mongoDB database .

--- api/controllers/hotels.controllers.js
+++ api/controllers/hotels.controllers.js
@@ -1,38 +1,84 @@
 var dbconn  = require('../data/dbconnection.js');
+var ObjectId = require('mongodb').ObjectId;
 var hotelData = require('../data/hotel-data.json');
-var db = dbconn.get();
+
 module.exports.hotelsGetAll = function(req, res) {
-    console.log('GET the json');
-    console.log(req.query);
+    var db = dbconn.get();
+    var collection = db.collection('hotels');
     var offset = 0;
     var count = 5;
     if(req.query&&req.query.offset)
     {
-    	offset = parseInt(req.query.offset,10);
+      offset = parseInt(req.query.offset,10);
     }
     if(req.query&&req.query.count)
     {
-    	count = parseInt(req.query.count,10);
+      count = parseInt(req.query.count,10);
     }
+    collection
+      .find()
+      .skip(offset)
+      .limit(count)
+      .toArray(function(err,docs){
+         console.log("Found hotels",docs);
+      res
+         .status(200)
+         .json(docs);
+      });
+   
+    /*console.log('GET the json');
+    console.log(req.query);
+
+    
     var returnData = hotelData.slice(offset,offset+count);
     res
       .status(200)
-      .json(returnData);
+      .json(returnData);*/
   }
 
 module.exports.hotelsGetOne = function(req, res) {
+    var db = dbconn.get();
+    var collection = db.collection('hotels');
     var hotelId = req.params.hotelId
-    var thisHotel = hotelData[hotelId];
-    console.log('GET hotel ID', hotelId);
+    // var thisHotel = hotelData[hotelId];
+    collection
+      .findOne({_id:ObjectId(hotelId)},function(err,doc){
+        console.log('GET hotel ID', hotelId);
     res
-      .status(200)	
-      .json(thisHotel);
+      .status(200)  
+      .json(doc);
+      });
+    
   }
 module.exports.hotelsAddOne = function(req,res)
 {
+    var db = dbconn.get();
+    var collection = db.collection('hotels');
 		console.log("POST new Hotel");
-		console.log("req.body");
-		res
-			.status(200)
-			.json(req.body)
+    var newHotel;
+    if(req.body&&req.body.name&&req.body.stars)
+    {
+      var newHotel = req.body;
+      newHotel.stars = parseInt(req.body.stars,10);
+      console.log("req.body");
+      collection
+        .insertOne(newHotel,function(err,response){
+        res
+          .status(201)
+          .json(response.ops);
+
+        });
+
+
+      
+    }
+    else
+    {
+      console.log("Data missing from body");
+      res
+        .status(400)
+        .json({"message":"Required data missing from the body"});
+
+    }
+		
 }
